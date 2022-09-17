ba83bc1366eefbf6956e6fd8205c28dc8977e791
Arzekeil-Abel De Leon <delearze@gmail.com>
2022-09-17 12:04:15 -0400

    loading cards works now

---
+++ client/src/components/BasicCard.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardActionArea from '@mui/material/CardActionArea';
+import Typography from '@mui/material/Typography';
+
+export default function BasicCard({ id, title, authors, recent_editors }) {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardActionArea href="https://google.com">
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Last edited by... {recent_editors}
+          </Typography>
+          <Typography variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            {authors}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
--- client/src/components/DashboardView.jsx
+++ client/src/components/DashboardView.jsx
@@ -1,9 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-export default function DashboardView () {
+import BasicCard from "./BasicCard";
+import LoadingComponent from "./LoadingComponent"
+
+export default function DashboardView() {
+    const [data, setData] = useState([{ "title": "Document", "authors": "John Doe", "recent_editors": "Jane Doe" }]);
+
+    useEffect(() => {
+        // axios({
+        //     method: 'get',
+        //     url: ''
+        //   })
+        //     .then(function (res) {
+        //         setData(res.data);
+        //     });
+    }, []);
+
+    if (data.length === 0) return <LoadingComponent />;
     return (
         <>
-            <h1>Good soup</h1>
+            {data.map((data_point) => {
+                return (
+                    <BasicCard
+                        key={data_point["title"]}
+                        id={id}
+                        title={data_point["title"]}
+                        authors={data_point["authors"]}
+                        recent_editors={data_point["recent_editors"]}
+                    />
+                );
+            })}
         </>
     );
 }
